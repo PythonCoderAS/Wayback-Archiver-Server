253c33a627015cc3cf2a954c9ee3746d12ab2ea0
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-21 13:48:21 -0400

    Fix host list

--- src/server/views/host.py
+++ src/server/views/host.py
@@ -7,17 +7,20 @@ from ..models.host import GeneratedHost, HostListPaginationResponse
 from ..utils.pagination import PaginationParams, PaginationResponse, apply_pagination_params, validate_pagination
 from ...models import Host
 
+
 @app.get("/api/host/{id}", response_model=GeneratedHost, responses={404: {"model": ErrorModel}})
 async def get_host(id: int):
-    host = await Host.get_or_none(id=id).annotate(count=Count("sessions__items"), session_count=Count("sessions"))
+    host: Host = await Host.get_or_none(id=id).annotate(count=Count("sessions__items"))
     if host is None:
         raise HTTPException(status_code=404, detail="Host not found.")
+    session_count = await host.sessions.all().count()
     return GeneratedHost(
         id=host.id,
         created_on=host.created_on.timestamp(),
         items=host.count or 0,
-        sessions=host.session_count or 0
+        sessions=session_count or 0
     )
+
 
 @app.get("/api/hosts", response_model=HostListPaginationResponse, responses={400: {"model": ErrorModel}})
 async def get_hosts(after: int = 0, limit: int = 100):
@@ -26,13 +29,16 @@ async def get_hosts(after: int = 0, limit: int = 100):
     true_base = Host.all()
     if limit != 0:
         base_qs = apply_pagination_params(true_base, params)
-        hosts = await base_qs.annotate(count=Count("sessions__items"), session_count=Count("sessions")).order_by("id")
+        hosts = await base_qs.annotate(count=Count("sessions__items")).order_by("id")
+        ids = {host.id for host in hosts}
+        sessions_mapping = {host.id: host.session_count for host in await Host.all().filter(id__in=ids).annotate(
+            session_count=Count("sessions"))}
         host_data = [
             GeneratedHost(
                 id=host.id,
                 created_on=host.created_on.timestamp(),
                 items=host.count or 0,
-                sessions=host.session_count or 0
+                sessions=sessions_mapping[host.id] or 0
             )
             for host in hosts
         ]
